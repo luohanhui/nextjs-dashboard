ac096945036ed85cf23f15298ed0029a136736e9
Luohanhui <835583557@qq.com>
2024-06-20 15:42:54 +0800

    添加 服务器组件验证信息

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import {
   CheckIcon,
@@ -11,6 +12,7 @@ import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 
 import { updateInvoice } from '@/app/lib/actions';
+import { useActionState,useState } from 'react';
 
 export default function EditInvoiceForm({
   invoice,
@@ -19,9 +21,13 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+
+
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
